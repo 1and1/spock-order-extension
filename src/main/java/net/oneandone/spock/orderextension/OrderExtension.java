d4f3ba3e7ae683faa9a47f707f2ffcfe2f67004c
Martin Hofmann-Sobik <martin.hofmannsobik@1und1.de>
2020-10-16 23:09:04 +0200

    Fix sonar

--- src/main/java/net/oneandone/spock/orderextension/OrderExtension.java
+++ src/main/java/net/oneandone/spock/orderextension/OrderExtension.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 public class OrderExtension extends AbstractAnnotationDrivenExtension<Order> {
 
+    @Override
     public void visitSpecAnnotation(Order annotation, final SpecInfo spec) {
         sortFeatures(spec, annotation);
         includeFeaturesBeforeLastIncludedFeature(spec);
